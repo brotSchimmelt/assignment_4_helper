0e76c2ff63e46f8a083fe9be55418316eef5c85c
brotSchimmelt <tknebler+git@googlemail.com>
2023-07-10 16:43:40 +0200

    add helper functions

---
+++ utils.py
@@ -0,0 +1,77 @@
+from typing import List
+
+import nltk
+import pandas as pd
+
+nltk.download("punkt")
+
+
+def check_for_empty_text_fields(
+    dataframes: List[pd.DataFrame], text_fields: List[str]
+) -> None:
+    """
+    Check for empty text fields in a list of DataFrames and print the results.
+
+    Args:
+        dataframes (List[pd.DataFrame]): A list of DataFrames to check for empty text fields.
+        text_fields (List[str]): A list of column names representing the text fields to check.
+
+    Returns:
+        None
+
+    Prints:
+        - If empty text fields are found, prints the DataFrame index and corresponding
+        text field names where empty fields are present.
+        - If no empty text fields are found, prints "No empty text fields found."
+    """
+    result = False
+    empty_fields = []
+    for idx, df in enumerate(dataframes):
+        for text_field in text_fields:
+            # check for empty rows
+            if df[text_field].isna().any():
+                result = True
+                empty_fields.append((f"Dataframe {idx}", text_field))
+
+    if result:
+        print(f"There are empty text fields. Found here: {empty_fields}")
+    else:
+        print("No empty text fields found.")
+
+    return None
+
+
+def get_dfs_for_exploration(dataframes: List[pd.DataFrame]) -> List[pd.DataFrame]:
+    """
+    Takes a list of DataFrames and calculates the length of the text fields and
+    the reduction in length between the argument and conclusion.
+
+    Args:
+        dataframes (List[pd.DataFrame]): A list of DataFrames to be processed.
+
+    Returns:
+        List[pd.DataFrame]: A list of processed DataFrames with additional columns added.
+    """
+    new_dfs = []
+    for df in dataframes:
+        df["argument_len"] = df["argument"].str.lower()
+        df["conclusion_len"] = df["conclusion"].str.lower()
+
+        # calculate length of text fields
+        df["argument_len"] = df["argument"].apply(len)
+        df["conclusion_len"] = df["conclusion"].apply(len)
+
+        # calculate reduction in length between argument and conclusion
+        df["length_reduction"] = abs(df["conclusion_len"] / df["argument_len"] - 1)
+
+        # count the number of sentences in the argument and conclusion
+        df["argument_sentence_count"] = df["argument"].apply(
+            lambda x: len(nltk.sent_tokenize(x))
+        )
+        df["conclusion_sentence_count"] = df["conclusion"].apply(
+            lambda x: len(nltk.sent_tokenize(x))
+        )
+
+        new_dfs.append(df)
+
+    return new_dfs
