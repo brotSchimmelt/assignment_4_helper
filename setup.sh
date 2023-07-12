4a59fb6a9c3e0f1a45c29d664196ef839217b6ec
brotSchimmelt <tknebler+git@googlemail.com>
2023-07-12 12:29:39 +0200

    fix permissions for pip

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 sudo mkdir -p /usr/local/cuda/lib64
 sudo ln -s /usr/lib/x86_64-linux-gnu/libcudart.so /usr/local/cuda/lib64/libcudart.so
 
-sudo pip3 install --upgrade pip
-sudo pip3 install gdown pandas numpy notebook
-sudo pip3 install --upgrade numexpr
+pip install --upgrade pip
+pip install gdown pandas numpy notebook
+pip install --upgrade numexpr
 
 jupyter notebook --no-browser --port=8080
